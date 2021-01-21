5f1003ceba412ba2a219d8760ed209b3e7b0fcd1
Marcus Vieira <marcusvinicius.vieira88@gmail.com>
2021-01-21 07:21:31 -0300

    Update generics examples

--- src/array-example.ts
+++ src/array-example.ts
@@ -1,13 +1,23 @@
-// No type-checking
-export function arrayAnyType(array: any): any {
-    array.pop();
-    console.log(`arrayAnyType value: ${array} length: ${array.length}`);
+// Duplicate code: for number array type
+export function removeLastNumber(array: number[]): number[] {
+    console.log(array.pop());
     return array;
 }
 
-// Most flexible and scalable approach
-export function arrayGenerics<T>(array: T[]): T[] {
-    array.pop();
-    console.log(`arrayGenerics value: ${array} length: ${array.length}`);
+// Duplicate code: for string array type
+export function removeLastString(array: string[]): string[] {
+    console.log(array.pop());
+    return array;
+}
+
+// Option to avoid code duplication: lost the type-checking (Runtime errors)
+export function removeLastItem(array: any): any {
+    console.log(array.pop());
+    return array;
+}
+
+// dynamic and reusable code
+export function removeLastArrayItem<T>(array: T[]): T[] {
+    console.log(array.pop());
     return array;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import { customArray } from "./class-example";
-import { arrayAnyType, arrayGenerics } from "./array-example";
+import { removeLastNumber, removeLastString, removeLastItem, removeLastArrayItem } from "./array-example";
 import { testGenerics, testTyped, testAnyType, Dog, Bird, Ghost } from "./inheritance-example";
 
 // GENERICS IN ARRAYS
-arrayAnyType(["AAA", "BBB", "CCC"])
-// arrayAnyType(19) // runtime error
+removeLastNumber([10, 20, 30])
+removeLastString(["AAA", "BBB", "CCC"])
+// removeLastItem(19) // runtime error
 
-arrayGenerics(["GGG", "HHH", "III"])
-arrayGenerics<Number>([12, 13, 14])
-// arrayGenerics(19) //compile error
-// arrayGenerics("It is not an array") //compile error
+removeLastArrayItem(["GGG", "HHH", "III"])
+removeLastArrayItem<Number>([12, 13, 14])
+// removeLastArrayItem(19) //compile error (type-checking)
 
 // GENERICS IN CLASSES
 let numberObject = new customArray<number>();
@@ -21,7 +21,6 @@ stringObject.addItem('Test Generics');
 console.log(stringObject);
 
 // GENERICS IN INHERITANCE
-
 testAnyType(new Bird())
 // testAnyType("I'm not an animal") // runtime error
 
